fe4891aa572219398aff07dfd3c4e2be243985d6
Gregory Senyshyn <gregory.senyshyn@gmail.com>
2016-01-31 15:20:46 -0500

    refactored and added QT5 gui

--- ttm.py
+++ ttm.py
@@ -1,62 +1,79 @@
-#!/usr/bin/env python3
-
 import re
 import io
+import time
 from smtplib import SMTP_SSL
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
 import PyPDF2
 
-
-'''Remember to enable access for "less secure devices" in your gmail settings!
-'''
-
-TIMETABLES_PDF = ''
+PDF_FILE = ''
 EMAIL_USER = ''
 EMAIL_PASS = ''
+EMAIL_SUBJECT = ''
 
 
-def get_email_address(oen, last_name, first_name):
-    return '{0}{1}{2}@ugcloud.ca'.format(first_name[:2],
-                                         last_name[:3],
-                                         oen.replace('-','')[5:])
+def strip_punctuation(full_name):
+    things_to_remove = ["'"]
+    for thing in things_to_remove:
+        full_name.replace(thing, "")
+    return full_name
 
 
-def send_email(email_address, pdf_io, smtp):
-    msg = MIMEMultipart()
-    msg['Subject'] = 'Your Timetable for Semester 2'
-    msg['To'] = email_address
-    pdf_io.seek(0)
-    msg.attach(MIMEApplication(pdf_io.read(), _subtype='PDF'))
-    smtp.send_message(msg)
+def get_email_address(oen, full_name):
+    full_name = strip_punctuation(full_name)
+    last_name, first_name = full_name.split(',')
+    return '{0}{1}{2}@ugcloud.ca'.format(first_name.strip().capitalize()[:2],
+                                         last_name.strip().capitalize()[:3],
+                                         oen.replace('-', '')[5:])
 
 
-def main(pdf_file=TIMETABLES_PDF):
-    reader = PyPDF2.PdfFileReader(open(pdf_file, 'rb'))
-    oen_re = re.compile('OEN Number: .*[0-9]{3}-[0-9]{3}-[0-9]{3}')
+def get_oen_re():
+    return re.compile('OEN Number: .*[0-9]{3}-[0-9]{3}-[0-9]{3}')
+
+
+def get_smtp(user, password):
     smtp = SMTP_SSL('smtp.gmail.com:465')
-    smtp.login(EMAIL_USER, EMAIL_PASS)
-    for page in reader.pages:
-        text = page.extractText()
-        match = oen_re.search(text)
-        if match:
-            oen = text[match.start()+11:match.end()].strip()
-            full_name = text[35:match.start()].strip()
-            if not full_name.count(',') == 1:
-                print('{0} has a weird name'.format(full_name))
-                continue
-            else:
-                last_name, first_name = full_name.split(',')
-            email_address = get_email_address(oen,
-                                              last_name.strip(),
-                                              first_name.strip())
-            writer = PyPDF2.PdfFileWriter()
-            writer.addPage(page)
-            pdf_io = io.BytesIO()
-            writer.write(pdf_io)
-            send_email(email_address, pdf_io, smtp)
-    smtp.close()
+    smtp.login(user, password)
+    return smtp
+
+
+def get_message(page, email_user, subject, oen_re):
+    text = page.extractText()
+    match = oen_re.search(text)
+    if match:
+
+        writer = PyPDF2.PdfFileWriter()
+        writer.addPage(page)
+        pdf_io = io.BytesIO()
+        writer.write(pdf_io)
+        pdf_io.seek(0)
+
+        oen = text[match.start()+11:match.end()].strip()
+        full_name = text[35:match.start()].strip()
+        full_name = strip_punctuation(full_name)
+        email_address = get_email_address(oen, full_name)
+
+        msg = MIMEMultipart()
+        msg['To'] = email_address
+        msg['From'] = email_user
+        msg['Subject'] = subject
+        msg.attach(MIMEApplication(pdf_io.read(), _subtype='PDF'))
+
+        return msg
+
+
+def main():
+    with open(PDF_FILE, 'rb') as f:
+        reader = PyPDF2.PdfFileReader(f)
+        oen_re = get_oen_re()
+        smtp = get_smtp(USER_EMAIL, USER_PASS)
+        for page in reader.pages:
+            msg = get_message(page, EMAIL_USER, EMAIL_SUBJECT, oen_re)
+            smtp.send_message(msg)
+            print('Sent email to: '.format(msg['To']))
+            time.sleep(2)
+        smtp.close()
 
 
 if __name__ == '__main__':
